dcb2bfb19c52a7fbd37d42e81e4399c191b3e950
Vivek Patel <vpatel20001@gmail.com>
2024-09-17 01:20:33 -0400

    buggy commit, not proud. ProjectBox.jsx and ProjectBox.css need some serious fixes.

--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -7,16 +7,17 @@ import './ProjectBox.css';
 const ProjectBox = ({ title, image, projectLink, githubLink }) => {
   return (
     <div className="project-box">
-      {/* Clickable box to navigate to the project page */}
       <Link to={projectLink} className="project-link">
         <div className="project-title">{title}</div>
         <div className="project-content">
           <img src={image} alt={title} className="project-image" />
         </div>
       </Link>
-      <a href={githubLink} target="_blank" rel="noopener noreferrer" className="github-link">
-        <FontAwesomeIcon icon={faGithub} className="github-icon" />
-      </a>
+      <div className="project-footer">
+        <a href={githubLink} target="_blank" rel="noopener noreferrer" className="github-link">
+          <FontAwesomeIcon icon={faGithub} className="github-icon" />
+        </a>
+      </div>
     </div>
   );
 };
